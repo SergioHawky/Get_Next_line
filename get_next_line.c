1cd623df2b1ee845a13389095b0e6febe8fc7bd2
SergioHawky <sergioilkiv14@gmail.com>
2025-01-24 21:04:51 +0000

    final

--- get_next_line.c
+++ get_next_line.c
@@ -8,14 +8,14 @@ char	*get_next_line(int fd)
 
 	line = NULL;
 	bytes_read = 1;
-	while ((fd >= 0 && fd < FOPEN_MAX && BUFFER_SIZE > 0) && bytes_read > 0)
+	while ((fd >= 0 && BUFFER_SIZE > 0) && bytes_read > 0)
 	{
 		if (buff[0])
 		{
-			line = str_join_buff(line, buff);
+			line = ft_strjoin(line, buff);
 			if (!line)
 				return (NULL);
-			if (line[ft_strlen(line, '\n') - 1] == '\n')
+			if (line[ft_strlen(line) - 1] == '\n')
 				break ;
 		}
 		else
@@ -29,19 +29,51 @@ char	*get_next_line(int fd)
 	return (line);
 }
 
-#include <fcntl.h>
-#include <stdio.h>
-int	main(int argc, char **argv)
+
+/*int	main(void)
 {
 	int		fd;
 	char	*line;
 
-	if (argc != 2)
+	// Abrir o ficheiro para leitura
+	fd = open("test_file.txt", O_RDONLY);
+	if (fd < 0)
+	{
+		perror("Erro ao abrir o ficheiro");
 		return (1);
-	fd = open(argv[1], O_RDONLY);
-	line = get_next_line(fd);
-	printf("!%s!\n", line);
-	line = get_next_line(fd);
-	printf("!%s!\n", line);
+	}
+
+	// Ler e imprimir linha por linha
+	while ((line = get_next_line(fd)) != NULL)
+	{
+		printf("%s", line);
+		free(line); // Libertar memória da linha retornada
+	}
+
+	// Fechar o ficheiro
+	printf("\n");
+	close(fd);
+	return (0);
+}*/
+
+int	main(void)
+{
+	int		fd;
+	char	*line;
+	char	buffer[1024+1];
+	int		read_bytes;
+
+	(void) line;
+	fd = open("test_file.txt", O_RDONLY);
+	(void) read_bytes;
+	buffer[1024] = '\0';
+	while(!(buffer[0] == 'E' && buffer[1] == '\n'))
+	{
+		printf("Quer uma nova linha, de enter to Exit (E):\n");
+		read_bytes = read(0,buffer,1024);
+		if(buffer[0] == 'E' && buffer[1] == '\n')
+			break ;
+		printf("%s", get_next_line(fd));
+	}
 	return (0);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -1,16 +1,17 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
-# include <stdlib.h>
-# include <stdio.h>
-
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 2
+#  define BUFFER_SIZE 1
 # endif
 
+# include <fcntl.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <unistd.h>
+
 char	*get_next_line(int fd);
-char	*str_join_buff(char *s1, char *s2);
-size_t	ft_strlen(char *str, char term);
+size_t	ft_strlen(char *str);
+char	*ft_strjoin(char *s1, char *s2);
 
 #endif
